3f1165eff49c1e48e859de5498549e1873c2bae1
Ema998 <emanuele.gornati3@gmail.com>
2025-05-06 13:37:54 +0200

    commit

---
+++ resources/views/prodotti-detail.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>gameland</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+      <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><i class="bi bi-controller"></i></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('aboutus')}}">Chi Siamo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('contatti')}}">Contatti</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('prodotti')}}">Prodotti</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <header class="container-fluid">
+        <div class="row h-100 align-items-center justify-content-center">
+            <div class="row">
+                <h2 class="text-center display-5">{{$prodotto['name']}}</h2>
+            </div>
+            <div class="col-md-6 div-col-12 justify-content-center align-items-center">
+                <h3 class="text-center">Nome:{{$prodotto['name']}}</h3>
+                <h4 class="text-cenetr">Descrizione:{{$prodotto['description']}}</h4>
+                <h4 class="text-cenetr">Prezzo:{{$prodotto['price']}}</h4>
+            </div>
+        </div>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
