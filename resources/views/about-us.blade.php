3b6b47df69c84d28cf127961861ee76d03cb358c
Ema998 <emanuele.gornati3@gmail.com>
2025-05-06 15:03:04 +0200

    commit

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,37 +1,4 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>about-us</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-        <link rel="stylesheet" href="/style.css">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><i class="bi bi-controller"></i></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="{{route('aboutUs')}}">Chi Siamo</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="{{route('contatti')}}">Contatti</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('prodotti')}}">Prodotti</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<x-layout>
         <header class="container-fluid">
             <div class="row h-100 justify-content-around align-items-center">
                 <div class="col-6">
@@ -44,18 +11,10 @@
                 <div class="row h-100 justify-content-around align-items-center">
                     @foreach ($users as $user)
                         <div class="col-12 col-md-4">
-                            <div class="card" style="width: 18rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$user['name'] . " " . $user['surname']}}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">$user['role']</h6>
-                                    <a href="{{route('aboutUsDetail', ['name'=>$user['name']])}}" class="card-link"> Scopri di più</a>
-                                </div>
-                            </div>
+                           <x-about-us-card userName="{{ $user['name'] }}" userSurname="{{ $user['surname']}}" userRole="{{ $user['role'] }}" />
                         </div>
                     @endforeach
                 </div>
             </div>
         </section>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-    </body>
-</html>
+</x-layout>
